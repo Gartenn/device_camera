1d4f8353fee811046628e0836936257826770e58
Garten <brilliangarten@gmail.com>
2026-02-21 19:34:11 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,5 +16,4 @@ CAMERA_PACKAGE_NAME := com.android.camera
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
 
 # Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/private
